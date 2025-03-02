4cb34cde18e7c2b723a4e1fc16ab04779a41f557
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-03-02 00:24:30 -0500

    Cleanup

--- vendor/rpurinton/config/src/Exceptions/ConfigException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace RPurinton\Exceptions;
-
-class ConfigException extends \RuntimeException {}
--- vendor/rpurinton/https/src/Exceptions/HTTPSException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace RPurinton\Exceptions;
-
-class HTTPSException extends \Exception {}
